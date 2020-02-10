894cb2d70049764cfa26df650340758f5c659d2f
riju@rpi3b <mailmeonriju@gmail.com>
2020-02-10 14:11:31 +0530

    removed 'IP:' text

--- start.py
+++ start.py
@@ -15,13 +15,13 @@ def main():
     # print(ip)
     logger.debug('checking IP on remote path')
     remote_location_ip = subprocess.check_output(['curl', config['REMOTE_IP']['URL_PREFIX'] + config['REMOTE_IP']['NODE_FILE']]).decode().strip()
-    if 'IP: ' in remote_location_ip:
-        remote_location_ip = remote_location_ip.split('IP: ')[1].split(' ')[0]
-        if remote_location_ip == ip:
-            logger.info('IP updated to remote location is same as current external IP')
-            return
-        else:
-            upload_ip(ip)    
+    # if 'IP: ' in remote_location_ip:
+    #     remote_location_ip = remote_location_ip.split('IP: ')[1].split(' ')[0]
+    if remote_location_ip == ip:
+        logger.info('IP updated to remote location is same as current external IP')
+        return
+    else:
+        upload_ip(ip)    
     else:
         logger.info('IP not updated to remote location')
         upload_ip(ip)
@@ -29,7 +29,7 @@ def main():
 def upload_ip(ip):
     logger.info('updating IP to remote location')
     with open(os.path.join(config['LOCAL_REPO']['PATH'], config['LOCAL_REPO']['NODE_FILE']), 'w') as out:
-        out.write('IP: ' + ip)
+        out.write(ip)
     # subprocess.run(['cd', config['LOCAL_REPO']['PATH']])
     subprocess.run(['git', 'add', os.path.join(config['LOCAL_REPO']['PATH'], config['LOCAL_REPO']['NODE_FILE'])], cwd=config['LOCAL_REPO']['PATH'])
     subprocess.run(['git', 'commit', '-m', 'updating external IP at {}'.format(int(time.time()))], cwd=config['LOCAL_REPO']['PATH'])
